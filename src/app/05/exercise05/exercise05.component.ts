eadf88bdb0045d6442e1b1695613431f720f0dc2
Pierre Chavaroche <info@chavaroche.de>
2020-08-05 06:57:36 +0200

    Renaming

--- src/app/05/exercise05/exercise05.component.ts
+++ src/app/05/exercise05/exercise05.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-exercise04',
-  templateUrl: './exercise04.component.html',
-  styleUrls: ['./exercise04.component.scss']
+  selector: 'app-exercise05',
+  templateUrl: './exercise05.component.html',
+  styleUrls: ['./exercise05.component.scss']
 })
-export class Exercise04Component implements OnInit {
+export class Exercise05Component implements OnInit {
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExerciseComponent } from './01/exercise/exercise.component';
 import { Exercise01Component } from './01/exercise01/exercise01.component';
 import { Exercise02Component } from './02/exercise02/exercise02.component';
 import { Exercise03Component } from './03/exercise03/exercise03.component';
-import { Exercise04Component } from './04/exercise04/exercise04.component';
+import { Exercise05Component } from './05/exercise05/exercise05.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExerciseComponent,
     Exercise01Component,
     Exercise02Component,
     Exercise03Component,
-    Exercise04Component
+    Exercise05Component
   ],
   imports: [
     BrowserModule,
