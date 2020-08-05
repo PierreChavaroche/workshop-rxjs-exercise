732994b0d0af001f2da1f7dc1bc07a2738aede1e
Pierre Chavaroche <info@chavaroche.de>
2020-08-05 07:59:27 +0200

    Renaming

--- src/app/06/exercise06/exercise06.component.ts
+++ src/app/06/exercise06/exercise06.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-exercise05',
-  templateUrl: './exercise05.component.html',
-  styleUrls: ['./exercise05.component.scss']
+  selector: 'app-exercise06',
+  templateUrl: './exercise06.component.html',
+  styleUrls: ['./exercise06.component.scss']
 })
-export class Exercise05Component implements OnInit {
+export class Exercise06Component implements OnInit {
 
   constructor() { }
 
